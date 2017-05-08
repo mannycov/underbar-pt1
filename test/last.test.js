faff161793b0c9fd7b3e8398c33d126344a2cd41
Emanuel <emanuel@Emanuels-MacBook-Pro.local>
2017-05-07 19:31:20 -0700

    added tests to last

--- test/last.test.js
+++ test/last.test.js
@@ -9,6 +9,14 @@ describe('last()', () => {
     expect(_.last(['a', 'b', 'c'], 2)).toEqual(['b', 'c']);
   });
 
+  it('returns the last 3 elements of an array', () => {
+    expect(_.last([1, 2, 3, 4, 5], 3)).toEqual([3, 4, 5]);
+  });
+
+  it('returns the whole array if you ask for the same number of elements that it has', () => {
+    expect(_.last([true, false, null, undefined], 4)).toEqual([true, false, null, undefined]);
+  });
+
   it('returns the whole array if you ask for more elements than it has', () => {
     expect(_.last(['a', 'b', 'c'], 5)).toEqual(['a', 'b', 'c']);
   });
