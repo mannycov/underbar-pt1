bf2f7889c32a78e3a6096823f535ca04cf0048b5
Emanuel <emanuel@Emanuels-MacBook-Pro.local>
2017-05-07 19:59:27 -0700

    added tests to map

--- test/map.test.js
+++ test/map.test.js
@@ -6,4 +6,19 @@ describe('map()', () => {
     const mappedArr = _.map(arr, (el) => el * el);
     expect(mappedArr).toEqual([1, 4, 9, 16, 25]);
   });
+
+  it('maps every number in an array of numbers to their product given a factor to multiply in the callback', () => {
+    const nums = [2, 3, 4];
+    const mappedNums = _.map(nums, (item) => item * 3);
+    expect(mappedNums).toEqual([6, 9, 12]);
+  });
+
+  it('maps property in an array-like object of values', () => {
+    const obj = {
+      pie: 'pumpkin',
+    };
+    const mappedObj = _.map(obj, (property) => obj.pie);
+    expect(mappedObj).toEqual(['pumpkin']);
+  });
+
 });
