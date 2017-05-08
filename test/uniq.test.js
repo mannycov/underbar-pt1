527d03069228655af0bffaa80913d18008f06b97
Emanuel <emanuel@Emanuels-MacBook-Pro.local>
2017-05-07 20:39:08 -0700

    added test to uniq

--- test/uniq.test.js
+++ test/uniq.test.js
@@ -5,4 +5,10 @@ describe('uniq()', () => {
     const nums = [2, 4, 5, 5, 7, 4, 10, 2];
     expect(_.uniq(nums)).toEqual([2, 4, 5, 7, 10]);
   });
+
+  it('de-dups a list of strings', () => {
+    const strings = ['apple', 'apple', 'monkey', 'monkey', 'monkey', 'wrench'];
+    expect(_.uniq(strings)).toEqual(['apple', 'monkey', 'wrench']);
+  });
+  
 });
